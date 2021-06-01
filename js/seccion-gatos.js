3c9e03b83c8d3b3323844b1ad2167f25613d99d0
Yenifer <k.dreams17@gmail.com>
2021-06-01 16:26:19 -0400

    MODIFICACIONES DE ANDRE

--- js/seccion-gatos.js
+++ js/seccion-gatos.js
@@ -5,9 +5,48 @@ $(document).ready(function() {
     var nroColumnasPorFila = 3;
     var selectorTablaHTML = "#lista-gatos"
     var url = "https://api.thecatapi.com/v1/images/search?limit="+nroTotalImagenes
+    var urlraza = "https://api.thecatapi.com/images/search?breed_id="
+7	
+8	
+9	
+    $('#fieldTipoContacto').change(function() {
+	
+        limpiar();
+	
+        if ($(this).val() == '2') {
 
+            // Do something for option "b"
+	
+	
+            //$("#lista-perros").remove();
+	
+            generar_galeria_imagenes(selectorTablaHTML, nroTotalImagenes, nroColumnasPorFila, urlraza + 2);
+
+        }
+
+        else{
     generar_galeria_imagenes(selectorTablaHTML, nroTotalImagenes, nroColumnasPorFila, url,"Cambiar Imagen");
+	
+            generar_galeria_imagenes(selectorTablaHTML, nroTotalImagenes, nroColumnasPorFila, urlraza);
+	
+        }
+	
+    });
+	
+    
+
 });
+	
+
+
+function limpiar() {
+	
+    var Table = document.getElementById("lista-perros");
+	
+    Table.innerHTML = "";
+	
+}
+
 
 function getBtnActionURL() {
     return "https://api.thecatapi.com/v1/images/search?limit=1";
--- js/seccion-perros.js
+++ js/seccion-perros.js
@@ -7,9 +7,69 @@ $(document).ready(function() {
     var selectorTablaHTML = "#lista-perros"
     var url = "https://api.thedogapi.com/v1/images/search?limit="+nroTotalImagenes
 
-    generar_galeria_imagenes(selectorTablaHTML, nroTotalImagenes, nroColumnasPorFila, url,"Cambiar Imagen");
-});
-
-function getBtnActionURL() {
-    return "https://api.thedogapi.com/v1/images/search?limit=1";
-}
+    var urlraza = "https://api.TheDogAPI.com/images/search?breed_id="
+   	
+   	
+   	
+        $('#fieldTipoContacto').change(function() {
+   	
+            limpiar();
+   	
+            if ($(this).val() == '2') {
+    
+                // Do something for option "b"
+    
+                //$("#lista-perros").remove();
+    
+                generar_galeria_imagenes(selectorTablaHTML, nroTotalImagenes, nroColumnasPorFila, urlraza + 2);
+    	
+            }
+    	
+            else{
+    	
+        generar_galeria_imagenes(selectorTablaHTML, nroTotalImagenes, nroColumnasPorFila, url,"Cambiar Imagen");
+    	
+                generar_galeria_imagenes(selectorTablaHTML, nroTotalImagenes, nroColumnasPorFila, urlraza);
+    	
+            }
+    	
+        });
+    	
+    	
+        
+    	
+    });
+    	
+    
+    	
+    	
+    	
+    //<table id="lista-perros" class="table text-center">
+    	
+    	
+    //</table>
+    	
+    	
+    function limpiar() {
+    	
+        var Table = document.getElementById("lista-perros");
+    	
+        Table.innerHTML = "";
+    	
+    }
+    	
+    	
+    	
+    function getBtnActionURL() {
+    	
+    function getBtnActionURL() {
+    	
+        return "https://api.thedogapi.com/v1/images/search?limit=1";
+    	
+        return "https://api.thedogapi.com/v1/images/search?limit=1";
+    	
+    }
+    	
+    }
+         
+    
